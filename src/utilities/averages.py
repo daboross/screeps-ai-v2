a6e2de026a3f4ec297a0e96fc14d730b1730834c
Dabo Ross <daboross@daboross.net>
2016-09-19 20:37:19 -0700

    Track bucket in averages.py, and remote bucket trends in py.cpu_avg() in console.

--- src/utilities/averages.py
+++ src/utilities/averages.py
@@ -8,6 +8,8 @@ __pragma__('noalias', 'Infinity')
 
 def start_main_loop():
     volatile_cache.volatile().set("main_loop_start_cpu", Game.cpu.getUsed())
+    clean_and_prepare_memory_array("bucket")
+    Memory.cpu_usage["bucket"].push(Game.cpu.bucket)
 
 
 def clean_and_prepare_memory_array(name):
@@ -37,7 +39,28 @@ def get_average(name, count=None):
         sum = 0
         for x in tick_array:
             sum += x
-        return sum / len(tick_array)
+        return round(sum / len(tick_array) * 100) / 100
+
+
+def get_average_range(name, start, end):
+    tick_array = Memory.cpu_usage and Memory.cpu_usage[name] or []
+    if end < 0 or start > len(tick_array) or end > len(tick_array) or not end < start:
+        return None
+    sum = 0
+    for i in range(len(tick_array) - end, len(tick_array) - start):
+        sum += tick_array[i]
+    return round(sum / len(tick_array) * 100) / 100
+
+
+def get_bucket_trend(end, start):
+    tick_array = Memory.cpu_usage and Memory.cpu_usage["bucket"]
+    if not tick_array or not end < start < len(tick_array):
+        return "not enough data"
+    value = tick_array[start] - tick_array[end]
+    if value > 0:
+        return "+{}".format(value)
+    else:
+        return value
 
 
 def get_average_visual():
@@ -55,18 +78,26 @@ def get_average_visual():
         "\nAvg Runtime: {}"
         "\nAvg creep #: {}"
         "\nAvg CPU/creep: {}"
-        "\n50 ticks:"
-        "\nAvg CPU: {}"
-        "\nAvg Runtime: {}"
-        "\nAvg creep #: {}"
-        "\nAvg CPU/creep: {}".format(
+        "\n50 ticks average:"
+        "\nCPU: {}"
+        "\t(Runtime: {})"
+        "\ncreep #: {}"
+        "\tCPU/creep: {}"
+        "\nBucket trends:"
+        "\n50: {}"
+        "\t100: {}"
+        "\t500: {}"
+        "\t1000: {}".format(
             with_cl,
             without_cl,
             creeps,
-            with_cl / creeps,
+            round(with_cl / creeps * 10) / 10,
             with_cl50,
             without_cl50,
             creeps50,
-            with_cl50 / creeps50
+            round(with_cl50 / creeps50 * 10) / 10,
+            get_bucket_trend(0, 50),
+            get_bucket_trend(0, 100),
+            get_bucket_trend(0, 1000),
         )
     )
