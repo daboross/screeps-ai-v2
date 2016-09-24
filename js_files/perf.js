7d19b73dd1073f7975764d5e6b76aaa7984c8d24
Dabo Ross <daboross@daboross.net>
2016-09-24 00:20:16 -0700

    Add more efficient Creep.getActiveBodyparts function

--- js_files/perf.js
+++ js_files/perf.js
@@ -4,9 +4,9 @@
 var originalFindPath = Room.prototype.findPath;
 var setup = false;
 
-module.exports = function(options) {
+module.exports = function (options) {
     if (!setup) {
-        Array.prototype.filter = function(callback, thisArg) {
+        Array.prototype.filter = function (callback, thisArg) {
             var results = [];
             var arr = this;
             for (var iterator = 0; iterator < arr.length; iterator++) {
@@ -17,20 +17,40 @@ module.exports = function(options) {
             return results;
         };
 
-        Array.prototype.forEach = function(callback, thisArg) {
+        Array.prototype.forEach = function (callback, thisArg) {
             var arr = this;
             for (var iterator = 0; iterator < arr.length; iterator++) {
                 callback.call(thisArg, arr[iterator], iterator, arr);
             }
         };
 
-        Array.prototype.map = function(callback, thisArg) {
+        Array.prototype.map = function (callback, thisArg) {
             var arr = this;
             var returnVal = [];
             for (var iterator = 0; iterator < arr.length; iterator++) {
                 returnVal.push(callback.call(thisArg, arr[iterator], iterator, arr));
             }
             return returnVal;
+        };
+
+        // This one is my own addition!
+        // default:
+        // function (type) {
+        //     return _.filter(this.body, i => i.hits > 0 && i.type == type).length;
+        // }
+
+        Creep.prototype.getActiveBodyparts = function (type) {
+            var total = 0;
+            for (var i = this.body.length; i-- > 0;) {
+                var x = this.body[i];
+                if (x.hits <= 0) {
+                    break;
+                }
+                if (x.type == type) {
+                    total += 1;
+                }
+            }
+            return total;
         };
 
         setup = true;
