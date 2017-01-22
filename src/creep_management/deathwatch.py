ba5f2d4373048482dd9c5b3fced7792fbdad87f3
Dabo Ross <daboross@daboross.net>
2017-01-22 15:18:59 -0800

    Add room name in which a creep may have been killed to deathwatch's output

--- src/creep_management/deathwatch.py
+++ src/creep_management/deathwatch.py
@@ -13,20 +13,23 @@ __pragma__('noalias', 'type')
 
 def start_of_tick_check():
     if Memory.deathwatch:
-        for name, room_name, threats in Memory.deathwatch:
+        for name, home_name, threats, room_name in Memory.deathwatch:
             if name not in Game.creeps:
                 if not _.every(threats, lambda t: t == INVADER_USERNAME or t == 'unknown'):
                     threats = ['an invader' if t == INVADER_USERNAME else t for t in threats]
-                    msg = '[death] {}, a {} of {}, died, likely at the hands of {}.'.format(
-                        name, _.get(Memory, ['creeps', name, 'role'], 'creep'),
-                        room_name,
+                    msg = '[death][{}] {}, a {} of {}, died in {}, likely at the hands of {}.'.format(
+                        Game.time,
+                        name,
+                        _.get(Memory, ['creeps', name, 'role'], 'creep'),
+                        home_name,
+                        room_name or "the universe",
                         ("{} or {}".format(', '.join(threats[:len(threats) - 1]),
                                            threats[len(threats) - 1])
                          if len(threats) > 1 else threats[0])
                     )
                     print(msg)
                     Game.notify(msg)
-                meta = _.get(Memory, ['rooms', room_name, 'meta'])
+                meta = _.get(Memory, ['rooms', home_name, 'meta'])
                 if meta:
                     meta.clear_next = 0
 
@@ -47,7 +50,8 @@ def mark_creeps(room):
         for creep in room.find(FIND_MY_CREEPS):
             Memory.deathwatch.append([
                 creep.name, creep.memory.home,
-                _(hostiles).map(lambda h: _.get(h, ['owner', 'username'], 'unknown')).uniq().value()
+                _(hostiles).map(lambda h: _.get(h, ['owner', 'username'], 'unknown')).uniq().value(),
+                    room.name,
             ])
     elif count > 0:
         for creep in room.find(FIND_MY_CREEPS):
@@ -55,5 +59,6 @@ def mark_creeps(room):
                 Memory.deathwatch.append([
                     creep.name, creep.memory.home,
                     _(hostiles).filter(lambda h: movement.chebyshev_distance_room_pos(h, creep) < 4)
-                        .map(lambda h: _.get(h, ['owner', 'username'], 'unknown')).uniq().value()
+                        .map(lambda h: _.get(h, ['owner', 'username'], 'unknown')).uniq().value(),
+                    room.name,
                 ])
